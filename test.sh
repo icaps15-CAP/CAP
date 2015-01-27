6c470e1179a38ee3ab43ce9270a1f1d78131ac8c
Masataro Asai <guicho2.71828@gmail.com>
2015-01-27 19:05:26 +0900

    fix

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 export FD_DIR=$(dirname $(readlink -ef $0))/downward
 
-./component-planner --dynamic-space-size 2000 --default -t 1800 -m 2000000 test/p20.pddl
+./component-planner --dynamic-space-size 2000 --default -t 1800 -m 2000000 test/assembly-mixed/p20.pddl
 
 
 
