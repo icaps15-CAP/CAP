faf7f41811673285afd27d2c38452f2e4505f648
Masataro Asai <guicho2.71828@gmail.com>
2024-04-01 18:02:25 -0400

    default test to ff+ff

--- test.sh
+++ test.sh
@@ -9,15 +9,13 @@ capdir=$(dirname $(readlink -ef $0))
 #### lama 1st iteration
 
 export ffff="--both-search ff-clean - --remove-component-problem-cost --remove-main-problem-cost"
-export fdfd="--add-macro-cost --preprocessor fd-latest-clean --alias lama-first - --main-search fd-latest-clean --alias lama -"
+export fdfd="--preprocessor fd-latest-clean --alias lama-first - --main-search fd-latest-clean --alias lama - --add-macro-cost"
+export fffd="--preprocessor ff-clean - --main-search fd-latest-clean --alias lama - --remove-component-problem-cost --add-macro-cost"
+
 export prpr='--add-macro-cost --both-search probe-clean -'
-
 export mv='--both-search marvin2-clean - --remove-component-problem-cost --remove-main-problem-cost'
-
 export yhfd="--add-macro-cost --preprocessor yahsp3-clean - --main-search fd-clean $lama2011 -"
-
 export yh="--add-macro-cost --both-search yahsp3-clean -"
-
 export yhar="--add-macro-cost --preprocessor yahsp3-clean - --main-search ipc-arvandherd-sat-clean -"
 export yhib="--add-macro-cost --preprocessor yahsp3-clean - --main-search ipc-ibacop-mco-clean -"
 
@@ -26,12 +24,27 @@ rm -r /tmp/lisptmp /tmp/captest /tmp/newtmp
 
 echo "results goes to /tmp/captest/"
 
-# gnu parallel
+# ff + ff
 parallel $MORE_OPTIONS --joblog test.log \
     ./runtest.sh \
     ::: 01 \
     ::: $capdir/test/*/ \
-    ::: "$fdfd"
+    ::: "$ffff"
+
+# # fd + fd
+# parallel $MORE_OPTIONS --joblog test.log \
+#     ./runtest.sh \
+#     ::: 01 \
+#     ::: $capdir/test/*/ \
+#     ::: "$ffff"
+#
+# # ff + fd
+# parallel $MORE_OPTIONS --joblog test.log \
+#     ./runtest.sh \
+#     ::: 01 \
+#     ::: $capdir/test/*/ \
+#     ::: "$fffd"
+
 
 #  --ipc-threads
 # --force-single-node-components --force-variable-factoring --compatibility
